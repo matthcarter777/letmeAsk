a5c0fc833b2da9b8e033cb4845aa7054cad6ea24
Mateus <mateushbsb@gmail.com>
2021-06-24 13:44:11 -0300

    auth page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from 'react';
 
-export function Button() {
-  const [ number, setNumber ] = useState(0);
+import '../styles/button.scss';
 
-  function handlerAdd() {
-    setNumber(number + 1);
-  }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
+export function Button(props: ButtonProps) {
   return(
-    <button onClick={handlerAdd}>Clique aqui {number} </button>
+    <button className="button" {...props} />
   )
 }
